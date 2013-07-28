f8771dfa1bb1d446c270ac7a9d124afef9d22097
Ian Stokes-Rees <ijstokes@continuum.io>
2013-07-28 14:54:24 -0400

    Updates to include DB import

--- datasnoop/settings.py
+++ datasnoop/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join(BASE_DIR, '../employees_db/employees.db'),
+        'NAME': os.path.join(BASE_DIR, 'employees_db/employees.db'),
     }
 }
 
